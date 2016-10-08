ef57dc44865be449a3d8b764ce868067604e9bd0
RominaMartin <rjmustbear@gmail.com>
2016-10-08 18:18:52 +0200

    Agrega elementos al personaje - Se agregan elementos como la pajarita, corbata o gafas. Se estipula que   no se superpongan elementos"

--- js/Character.js
+++ js/Character.js
@@ -1,16 +1,47 @@
 function Character () {
+    
+    /* Elementos del personaje */
     this.skinElements = $('.skin');
     this.shirtElements = $('.shirt');
-    this.bowElements = $('.bow');
-    this.tieElements = $('.tie');
+    this.bow = document.getElementById('bow');
+    this.tie = document.getElementById('tie');
     this.beltElements = $('.belt');
     this.pantsElements = $('.pants');
+
+    this.glasses = document.getElementById('glasses');
+    this.hat = document.getElementById("hat");
+    this.guitar = document.getElementById('guitar');
+    this.suitcase = document.getElementById('suitcase');
+    this.moustacheOne = document.getElementById("firstmoustache");
+    this.moustacheTwo = document.getElementById("secondmoustache");
+    this.moustacheThree = document.getElementById("thirdmoustache");
+    this.moustacheFour = document.getElementById("fourthmoustache");
+    this.moustacheFive = document.getElementById("fifthmoustache");
     
-    this.tieCircles = $("#tiePoints");
-    this.glasses = $('#glasses');
-    this.moustaches = $('#moustaches');
-    this.hat = $("#hat");
-    this.buckle = $('#buckle');
-    this.guitar = $('#guitar');
-    this.suitcase = $('#suitcase');
+    /* Elementos para el control del personaje */
+    this.buttonGlasses = document.getElementById("buttonglasses");
+    this.buttonTie = document.getElementById("buttontie");
+    this.buttonBow = document.getElementById("buttonbow");
+    this.buttonHat = document.getElementById("buttonhat");
+    this.buttonMoustacheOne = document.getElementById("buttonfirstmoustache");
+    this.buttonMoustacheTwo = document.getElementById("buttonsecondmoustache");
+    this.buttonMoustacheThree = document.getElementById("buttonthirdmoustache");
+    this.buttonMoustacheFour = document.getElementById("buttonfourthmoustache");
+    this.buttonMoustacheFive = document.getElementById("buttonfifthmoustache");
+    
+    this.setDefaultStyle();
+
+};
+
+Character.prototype.setDefaultStyle = function () {
+    this.moustacheOne.style.visibility = "hidden";
+    this.moustacheTwo.style.visibility = "hidden";
+    this.moustacheThree.style.visibility = "hidden";
+    this.moustacheFour.style.visibility = "hidden";
+    this.moustacheFive.style.visibility = "hidden";
+
+    this.glasses.style.visibility = "hidden";
+    this.tie.style.visibility = "hidden";
+    this.bow.style.visibility = "hidden";
+    this.hat.style.visibility = "hidden";
 };
--- js/dressItUp.js
+++ js/dressItUp.js
@@ -1,5 +1,6 @@
 function dressItUp () {
-/**
+    var self = this;
+    /**
      * Personaje sobre el que se va a trabajar
      * @type Character
      */
@@ -10,17 +11,61 @@ function dressItUp () {
      * @type Character
      */
     var broccoliSaved = new Character();
+    
+    var show = function (element) {
+        element.style.visibility = "visible";
+    };
+    
+    var hide = function (element) {
+        element.style.visibility = "hidden";
+    };
+
+    /**
+     * Se asegura de no superponer elementos
+     * @returns {undefined}
+     */
+    var addElement = function () {
+        if (this.id.substring(0, 6) == "button") {
+            if(this.id.length > 13) { // Its a moustache
+                hide(broccoli.moustacheOne);
+                hide(broccoli.moustacheTwo);
+                hide(broccoli.moustacheThree);
+                hide(broccoli.moustacheFour);
+                hide(broccoli.moustacheFive);
+            } else if (this.id === "buttonbow" || this.id === "buttontie") {
+                hide(broccoli.bow);
+                hide(broccoli.tie);
+            }
+            show(document.getElementById(this.id.substring(6)));
+        }
+    };
+    
+    /**
+     * Listeners para agregar elementos desde los botonces de control
+     */
+    broccoli.buttonGlasses.addEventListener("click", addElement);
+    broccoli.buttonTie.addEventListener("click", addElement);
+    broccoli.buttonBow.addEventListener("click", addElement);
+    broccoli.buttonHat.addEventListener("click", addElement);
+    broccoli.buttonMoustacheOne.addEventListener("click", addElement);
+    broccoli.buttonMoustacheTwo.addEventListener("click", addElement);
+    broccoli.buttonMoustacheThree.addEventListener("click", addElement);
+    broccoli.buttonMoustacheFour.addEventListener("click", addElement);
+    broccoli.buttonMoustacheFive.addEventListener("click", addElement);
+
 };
 
 dressItUp.prototype.restore = function () {
     broccoli = broccoliSaved;
 };
 
-dressItUp.prototype.addElement = function (element) {
-  element.removeClass("hide");
+dressItUp.prototype.addElement = function () {
+    this.style.visibility = "visible";
 };
 
-dressItUp.prototype.hideElement = function (element) {
-    element.addClass("hide");
+dressItUp.prototype.hideElement = function () {
+    this.style.visibility = "hidden";
 };
+
+var broccoli = new dressItUp();
    
